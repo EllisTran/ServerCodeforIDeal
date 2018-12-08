bcfca205cf45f2ba89d92d4363c4d4a151c1e34e
EllisTran <41351491+EllisTran@users.noreply.github.com>
2018-12-07 20:01:11 -0600

    Add files via upload
    Added ... animation to server, also commented most things

--- JavaClient.java
+++ JavaClient.java
@@ -6,23 +6,29 @@ import java.io.*;
 		public static void main(String[] args) 
 		{  
 
-			Scanner scan = new Scanner(System.in);
+			Scanner scan = new Scanner(System.in);	//Allows for input from the terminal
 			try
 			{      
-				Socket soc=new Socket("localhost",2004);  
+				Socket soc=new Socket("localhost",2004);  			//Creates new socket to connect with server
+
+				//ALlows data to be received and sent to the server
 				DataOutputStream dout=new DataOutputStream(soc.getOutputStream()); 
 				DataInputStream din=new DataInputStream(soc.getInputStream());
+
 				System.out.print("Enter your ID number: ");
-				String id = scan.next();
-				scan.close();
-				dout.writeUTF(id);
+				String id = scan.next();		//Takes input into id
+				scan.close();		//stops input
+	
+				dout.writeUTF(id);		//Sends id to server
 				dout.flush();
+
+				//Receives data from Server
 				String idfromServer = din.readUTF();
 				String fullName = din.readUTF();
 				String email = din.readUTF();
-				System.out.println("\nName: " + fullName);
-				System.out.println("Universiy ID: " + idfromServer);
-				System.out.println("Email: " + email);
+				System.out.println("\nName:\t\t" + fullName);
+				System.out.println("Universiy ID:\t" + idfromServer);
+				System.out.println("Email:\t\t" + email);
 				din.close();
 				dout.close(); 
 				soc.close();
