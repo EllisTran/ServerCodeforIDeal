a1e67f7f698be50711fbabfae3147a747f44b609
EllisTran <41351491+EllisTran@users.noreply.github.com>
2018-12-07 20:02:43 -0600

    Add files via upload

--- PythonServer.py
+++ PythonServer.py
@@ -1,67 +1,41 @@
 import json
-data = {}
-t = []
-data['student'] = []
-data['people'] = []
 import time, sys
 import threading
-import socket           # Import socket module
+from Threading import Threading
+import socket                 # Import socket module
+data = {}
 soc = socket.socket()         # Create a socket object
-host = "localhost" # Get local machine name
-port = 2004                # Reserve a port for your service.
+host = "localhost"            # Get local machine name
+port = 2004                   # Reserve a port for your service.
 waitingforClient = True
-#serverSocket = socket(AF_INET, SOCK_STREAM)
-soc.bind((host, port))       # Bind to the port
+soc.bind((host, port))        # Bind to the port
 soc.listen(5)                 # Now wait for client connection.
 notFound = "Sorry, ID number not found"
 found = False
 
-index = 0
-global stillThreading
-stillThreading = True
+print("<------------------------------------------------------------------------->")
+print("                     WELCOME TO ELLIS'S PYTHON SERVER                      ")
+print("<------------------------------------------------------------------------->\n")
 
-
-print("\n<------------------------------------------------------------------------->\n")
-
-def waiting(stringWaiting, index):
-    global stillThreading
-    while stillThreading == True:
-        x = index % 4
-        dots = x *'.' + (3-x) * ' '
-        sys.stdout.write("\r" + stringWaiting + dots)
-        time.sleep(0.5)
-        sys.stdout.flush()
-        index+=1
-    stillThreading = True
-
-    
-
-def Threading(stringWaiting):
-    t
-     = threading.Thread(target=waiting, args = (stringWaiting, 0))
-    print(threading.active_count())
-    t.start()
-
-Threading("Waiting for connection from client")
+t = Threading("Waiting for connection from client", True)       #Create a new thread in the Threading class to do the ... animation
 
 while True:
     conn, addr = soc.accept()     # Establish connection with client.
-    stillThreading = False
-    index +=1
+    t.stillThreading = False
+    
     print ("\nGot connection from",addr,"\n")
-    print(threading.active_count())
-    Threading("Waiting to receive ID number from client")
-    print(threading.active_count())
+    t = Threading("Waiting for client to send student's ID number", True)
+    
     msg = conn.recv(1024)                   #Receive message from Client
     msg = msg.decode('UTF-8')               #Decode that and put it in Unicode
     msg = msg[2:]                           #Cut off the beginning of the UTF-8 message
-    stillThreading = False
 
-    print("\n","\nID number received:",msg)
+    t.stillThreading = False
+
+    print("\nID number received:",msg)
     with open('MOCK_DATA.json') as json_file: #Opens file as json
         print("Searching through JSON file...","\n")
-        data = json.load(json_file) #loads the information and puts it into the data list
-        #print(json.dumps(data, indent = 4)+ "\n")
+        data = json.load(json_file)     #loads the information and puts it into the data list
         for p in data['people']:        #Looks through all of the people in the JSON file to see if any of the id numbers match
             if msg == (p['id']):
                     #Parses JSON file to set variables equal to the needed values
@@ -69,9 +43,9 @@ while True:
                     firstName = p['first_name']
                     lastName = p['last_name']
                     email = p['email']
-                    print('Name: ' + firstName + ' ' + lastName)
-                    print('University ID: ' + msg)
-                    print('Email: ' + email)
+                    print('Name:\t\t' + firstName + ' ' + lastName)
+                    print('University ID:\t' + msg)
+                    print('Email:\t\t' + email)
 
                     #Encodes the variables back to UTF-8 and sends back to the client
                     id = id.encode('UTF-8')
@@ -85,17 +59,26 @@ while True:
                     conn.send(len(email).to_bytes(2, byteorder= 'big'))
                     conn.send(email)
                     found = True
+
         #Error Checking
         if found == False:
+            print(notFound)
             notFound = notFound.encode('UTF-8')
             conn.send(len(notFound).to_bytes(2, byteorder = 'big'))
             conn.send(notFound)
-            print("Sorry, ID number not found")
+            
     break
 #Closes Socket
 soc.close()
-print(threading.active_count())
 print("\n<------------------------------------------------------------------------->")
+print("                     ELLIS'S PYTHON SERVER SAYS GOODBYE                     ")
+print("<------------------------------------------------------------------------->\n")
+
+print("<------------------------------------------------------------------------->")
+print("Created By: \tEllis Tran")
+print("Date Created: \tDecemeber 2, 2018")
+print("Last Edited: \tDecember 7, 2018")
+print("<------------------------------------------------------------------------->")
     
  
 	
